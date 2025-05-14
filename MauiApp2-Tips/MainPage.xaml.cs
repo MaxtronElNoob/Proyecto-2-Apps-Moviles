53ef696569b0d88359cb6ac55f02df62ebfd7611
Alonso Herrera <alonso.bkn.098@gmail.com>
2025-05-14 18:32:36 -0400

    Avances se añadieron el tip por boton y los numeros que se ocuparan

--- MauiApp2-Tips/MainPage.xaml.cs
+++ MauiApp2-Tips/MainPage.xaml.cs
@@ -11,30 +11,51 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		viewModel = new UserViewModel();
 		BindingContext = viewModel;
+		//viewModel.Page = this;
+	}
+	public void Alert(string message)
+	{
+		DisplayAlert("Alert", message, "OK");
 	}
 }
 
 public partial class UserViewModel : ObservableObject
 {
-	[ObservableProperty]
-	private string name;
-
-	[ObservableProperty]
-	private string email;
-
+	public IRelayCommand<string> UpdateTipCommand2 { get; }
 	public UserViewModel()
 	{
-		// Inicializamos con datos por defecto
-		Name = "azul";
-		Email = "";
+		UpdateTipCommand2 = new RelayCommand<string>(UpdateTip);
 	}
-	[RelayCommand]
-	public void LoadUser(){
+	//public MainPage Page { get; set; }
+	[ObservableProperty]
+	private int people = 1;
+	[ObservableProperty]
+	private int tip_porcentage;
+	[ObservableProperty]
+	private int total_personal;
+	[ObservableProperty]
+	private int valor_boleta;
+	[ObservableProperty]
+	private int subtotal;
+	[ObservableProperty]
+	private int tip;
 
-		// Inicializamos con datos por defecto
-		Name = "Juan Perez";
-		Email = "juan@example.com";
-
+	private void UpdateTip(string? Button_porcentage){
+		Tip_porcentage = int.Parse(Button_porcentage??"0");
+		UpdateTotal();
 	}
+	
+	public void UpdateTotal(){
+		Subtotal = Valor_boleta / People;
+		Tip = (int)(Subtotal*(double)Tip_porcentage/100);
+		Total_personal = Tip + Valor_boleta;
+	}
+	// private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+	// {
+	// 	ViewModel.Equals.
+	// 	UserViewModel.UpdateTip((int) e.NewValue);
+		
+	// }
+
 }
 
